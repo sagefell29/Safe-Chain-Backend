36f8fc5c5782ce83f089d9b630e13974c95d4ded
prasoonsoni <prasoonsoni03@gmail.com>
2022-08-06 19:01:40 +0530

    Added function to store data on ipfs

--- controllers/passwordsController.js
+++ controllers/passwordsController.js
@@ -1,9 +1,10 @@
 const encryption = require('../scripts/encryption')
 const decryption = require('../scripts/decryption')
-const { v4: uuidv4 } = require('uuid')
-const fs = require('fs')
+const IPFS = require('ipfs-mini')
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+const User = require('../models/User')
 
-const savePassword = (req, res) => {
+const savePassword = async (req, res) => {
     try {
         const { name, website, username, password, description, token } = req.body
         const data = {
@@ -14,6 +15,15 @@ const savePassword = (req, res) => {
             description
         }
         const encrpytedData = encryption(data, token)
+        const cid = await ipfs.add(encrpytedData)
+        if (!cid) {
+            return res.json({ success: false, message: "Error Saving Password. Try Again Later." })
+
+        }
+        const update = await User.updateOne({ token: token }, { $push: { passwords: cid } })
+        if (!update.acknowledged) {
+            return res.json({ success: false, message: "Error Saving Password. Try Again Later." })
+        }
         res.json({ success: true, message: "Password saved successfully", data: encrpytedData })
     } catch (error) {
         console.log(error.message)
@@ -23,7 +33,7 @@ const savePassword = (req, res) => {
 
 const getPassword = (req, res) => {
     try {
-        const { text, token } = req.body
+        const { token } = req.body
         const decryptedData = decryption(text, token)
         res.json({ success: true, message: "Password retrieved successfully", data: decryptedData })
     } catch (error) {
